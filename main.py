f242f6a658675ad06e9a438621644d31056448e3
Andrew Skalicky <andrew064873@gmail.com>
2026-01-13 00:44:23 -0600

    wrote wordle function using enumerate to output and initial test for no matches

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+
 def generate_word():
     return "SOURCE"
+
+def wordle(guessedWord):
+    wordToGuess = generate_word()
+    
+    guessResult = ""
+    
+    for index, letter in enumerate(guessedWord):
+            if (index, letter) in enumerate(wordToGuess):
+                guessResult += "G"
+            elif letter in wordToGuess:
+                guessResult += "Y"
+            else:
+                guessResult += "-"
+
+                
+    return guessResult
+        
+print(wordle("PLANT"))
--- test_wordle.py
+++ test_wordle.py
@@ -3,3 +3,6 @@ import pytest
 
 def test_sample():
     assert generate_word() == "SOURCE"
+    
+def test_no_match():
+    assert wordle("PLANTY") == "------"
